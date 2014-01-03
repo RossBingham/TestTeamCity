c82714a71b9ede7eeea0c9246e63b3896517afec
RossBingham <ross@c4.net>
2014-01-03 13:24:48 -0600

    Adding two additional failures

---
+++ TestFX/TestFX/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFX
+{
+    public class Class3
+    {
+        public static void TestFailure()
+        {
+            
+        }
+    }
+}
